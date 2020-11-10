6b63d46416f08770a098cecdb5774fb4a8be425e
Trynda-LOL <40008415+Trynda-LOL@users.noreply.github.com>
2020-11-10 15:57:53 -0300

    aqrivos com funções e os objetos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: '38111', 
+                nome: 'Fuladno de tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '38111.png', legenda: 'Verador' }
+                ]
+            },
+            { 
+                numero: '77222', 
+                nome: 'Beltrano da silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Verador' }
+                ]
+            },
+        ]
+    },
+    { 
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99', 
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            { 
+                numero: '84', 
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
